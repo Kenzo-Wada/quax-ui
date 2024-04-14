63b5a4c10d18282d420113fb73c92967b7ec6f13
“Kenzo-Wada” <used.acormhib@gmail.com>
2024-04-15 00:59:35 +0900

    feat(packages/core): :tada: create base of `@qwik-ui/core`

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import pkg from "./package.json";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import tsconfigPaths from "vite-tsconfig-paths";
+const { dependencies = {}, peerDependencies = {} } = pkg as any;
+const makeRegex = (dep) => new RegExp(`^${dep}(/.*)?$`);
+const excludeAll = (obj) => Object.keys(obj).map(makeRegex);
+
+export default defineConfig(() => {
+  return {
+    resolve: {
+      alias: {
+        "~/": `${__dirname}/src/`,
+      },
+    },
+    build: {
+      sourcemap: true,
+      target: "es2020",
+      lib: {
+        entry: "./src/index.ts",
+        formats: ["es", "cjs"],
+        fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
+      },
+      rollupOptions: {
+        // externalize deps that shouldn't be bundled into the library
+        external: [
+          /^node:.*/,
+          ...excludeAll(dependencies),
+          ...excludeAll(peerDependencies),
+        ],
+      },
+    },
+    plugins: [qwikVite(), tsconfigPaths()],
+  };
+});
